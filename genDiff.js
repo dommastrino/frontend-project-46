d9542c612661d2b70511cc05656f2eaee54ba2ba
Бабкина <babkinaolga2000@gmail.com>
2022-09-04 15:28:22 +0300

    second step

--- genDiff.js
+++ genDiff.js
@@ -1,4 +1,5 @@
 import { Command } from 'commander';
 const commander = new Command();
 commander.version('1.0.0').description('Compares two configuration files and shows a difference.');
+commander.option('-f, --format <type>', 'output format').argument('<filepath1> <filepath2>');
 commander.parse(process.argv);
