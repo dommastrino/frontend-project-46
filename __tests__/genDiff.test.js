19961fce94070dab2cf868cfa70d9ee0d84a2ff0
Бабкина <babkinaolga2000@gmail.com>
2022-10-21 18:28:20 +0300

    tests

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,20 +1,24 @@
 import * as fs from 'node:fs';
 import * as path from 'node:path';
+import { fileURLToPath } from 'url';
 import { findDiff } from '../src/findDiff.js';
 
-test('testing function gendiff() with json files', () => {
-  const actual = findDiff('__fixtures__/data1.json', '__fixtures__/data2.json');
-  expect(actual).toEqual(fs.readFileSync(path.resolve(process.cwd(), '__fixtures__/resultStylish.txt'), 'utf-8'));
-  const actual2 = findDiff('__fixtures__/data1.json', '__fixtures__/data2.json', 'plain');
-  expect(actual2).toEqual(fs.readFileSync(path.resolve(process.cwd(), '__fixtures__/resultPlain.txt'), 'utf-8'));
-  const actual3 = findDiff('__fixtures__/data1.json', '__fixtures__/data2.json', 'json');
-  expect(actual3).toEqual(fs.readFileSync(path.resolve(process.cwd(), '__fixtures__/resultJson.txt'), 'utf-8'));
-});
-test('testing function gendiff() with yaml files', () => {
-  const actual = findDiff('__fixtures__/data1.yml', '__fixtures__/data2.yaml');
-  expect(actual).toEqual(fs.readFileSync(path.resolve(process.cwd(), '__fixtures__/resultStylish.txt'), 'utf-8'));
-  const actual2 = findDiff('__fixtures__/data1.yml', '__fixtures__/data2.yaml', 'plain');
-  expect(actual2).toEqual(fs.readFileSync(path.resolve(process.cwd(), '__fixtures__/resultPlain.txt'), 'utf-8'));
-  const actual3 = findDiff('__fixtures__/data1.yml', '__fixtures__/data2.yaml', 'json');
-  expect(actual3).toEqual(fs.readFileSync(path.resolve(process.cwd(), '__fixtures__/resultJson.txt'), 'utf-8'));
+const getPath = (filename) => path.resolve(path.dirname(fileURLToPath(import.meta.url)), './..', '__fixtures__', filename);
+describe('genDiff module', () => {
+  it('should be work with json', () => {
+    const actual = findDiff(getPath('data1.json'), getPath('data2.json'));
+    expect(actual).toEqual(fs.readFileSync(path.resolve(process.cwd(), getPath('resultStylish.txt')), 'utf-8'));
+    const actual2 = findDiff(getPath('data1.json'), getPath('data2.json'), 'plain');
+    expect(actual2).toEqual(fs.readFileSync(path.resolve(process.cwd(), getPath('resultPlain.txt')), 'utf-8'));
+    const actual3 = findDiff(getPath('data1.json'), getPath('data2.json'), 'json');
+    expect(actual3).toEqual(fs.readFileSync(path.resolve(process.cwd(), getPath('resultJson.txt')), 'utf-8'));
+  });
+  it('should be work with ymal', () => {
+    const actual = findDiff(getPath('data1.yml'), getPath('data2.yaml'));
+    expect(actual).toEqual(fs.readFileSync(path.resolve(process.cwd(), getPath('resultStylish.txt')), 'utf-8'));
+    const actual2 = findDiff(getPath('data1.yml'), getPath('data2.yaml'), 'plain');
+    expect(actual2).toEqual(fs.readFileSync(path.resolve(process.cwd(), getPath('resultPlain.txt')), 'utf-8'));
+    const actual3 = findDiff(getPath('data1.yml'), getPath('data2.yaml'), 'json');
+    expect(actual3).toEqual(fs.readFileSync(path.resolve(process.cwd(), getPath('resultJson.txt')), 'utf-8'));
+  });
 });
