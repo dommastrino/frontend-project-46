356fc929da0882a31c5fdd7cbc287b25be3fb98b
Бабкина <babkinaolga2000@gmail.com>
2022-10-25 18:37:13 +0300

    изменены тесты

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -11,11 +11,13 @@ const jsonData = fs.readFileSync(path.resolve(process.cwd(), getPath('resultJson
 describe('genDiff module', () => {
   it('should be work with json', () => {
     expect(findDiff(getPath('data1.json'), getPath('data2.json'))).toEqual(stylishData);
+    expect(findDiff(getPath('data1.json'), getPath('data2.json'), 'stylish')).toEqual(stylishData);
     expect(findDiff(getPath('data1.json'), getPath('data2.json'), 'plain')).toEqual(plainData);
     expect(findDiff(getPath('data1.json'), getPath('data2.json'), 'json')).toEqual(jsonData);
   });
   it('should be work with ymal', () => {
     expect(findDiff(getPath('data1.yml'), getPath('data2.yaml'))).toEqual(stylishData);
+    expect(findDiff(getPath('data1.yml'), getPath('data2.yaml'), 'stylish')).toEqual(stylishData);
     expect(findDiff(getPath('data1.yml'), getPath('data2.yaml'), 'plain')).toEqual(plainData);
     expect(findDiff(getPath('data1.yml'), getPath('data2.yaml'), 'json')).toEqual(jsonData);
   });
