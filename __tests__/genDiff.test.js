1e2ebd66159ca236cd63e89f10de3ffb426bbf22
Бабкина <babkinaolga2000@gmail.com>
2022-10-25 18:29:06 +0300

    изменены тесты

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,24 +1,23 @@
-import * as fs from 'node:fs';
-import * as path from 'node:path';
-import { fileURLToPath } from 'url';
-import findDiff from '../src/findDiff.js';
+import * as fs from "node:fs";
+import * as path from "node:path";
+import { fileURLToPath } from "url";
+import findDiff from "../src/findDiff.js";
 
-const getPath = (filename) => path.resolve(path.dirname(fileURLToPath(import.meta.url)), './..', '__fixtures__', filename);
-describe('genDiff module', () => {
-  it('should be work with json', () => {
-    const actual = findDiff(getPath('data1.json'), getPath('data2.json'));
-    expect(actual).toEqual(fs.readFileSync(path.resolve(process.cwd(), getPath('resultStylish.txt')), 'utf-8'));
-    const actual2 = findDiff(getPath('data1.json'), getPath('data2.json'), 'plain');
-    expect(actual2).toEqual(fs.readFileSync(path.resolve(process.cwd(), getPath('resultPlain.txt')), 'utf-8'));
-    const actual3 = findDiff(getPath('data1.json'), getPath('data2.json'), 'json');
-    expect(actual3).toEqual(fs.readFileSync(path.resolve(process.cwd(), getPath('resultJson.txt')), 'utf-8'));
+const getPath = (filename) => path.resolve(path.dirname(fileURLToPath(import.meta.url)), "./..", "__fixtures__", filename);
+const stylishData = fs.readFileSync(path.resolve(process.cwd(), getPath("resultStylish.txt")), "utf-8");
+const plainData = fs.readFileSync(path.resolve(process.cwd(), getPath("resultPlain.txt")), "utf-8");
+const jsonData = fs.readFileSync(path.resolve(process.cwd(), getPath("resultJson.txt")), "utf-8");
+
+describe("genDiff module", () => {
+  it("should be work with json", () => {
+    //expect(findDiff(getPath("data1.json"), getPath("data2.json"))).toEqual(fs.readFileSync(path.resolve(process.cwd(), getPath("resultStylish.txt")), "utf-8"));
+    expect(findDiff(getPath("data1.json"), getPath("data2.json"))).toEqual(stylishData);
+    expect(findDiff(getPath("data1.json"), getPath("data2.json"), "plain")).toEqual(plainData);
+    expect(findDiff(getPath("data1.json"), getPath("data2.json"), "json")).toEqual(jsonData);
   });
-  it('should be work with ymal', () => {
-    const actual = findDiff(getPath('data1.yml'), getPath('data2.yaml'));
-    expect(actual).toEqual(fs.readFileSync(path.resolve(process.cwd(), getPath('resultStylish.txt')), 'utf-8'));
-    const actual2 = findDiff(getPath('data1.yml'), getPath('data2.yaml'), 'plain');
-    expect(actual2).toEqual(fs.readFileSync(path.resolve(process.cwd(), getPath('resultPlain.txt')), 'utf-8'));
-    const actual3 = findDiff(getPath('data1.yml'), getPath('data2.yaml'), 'json');
-    expect(actual3).toEqual(fs.readFileSync(path.resolve(process.cwd(), getPath('resultJson.txt')), 'utf-8'));
+  it("should be work with ymal", () => {
+    expect(findDiff(getPath("data1.yml"), getPath("data2.yaml"))).toEqual(stylishData);
+    expect(findDiff(getPath("data1.yml"), getPath("data2.yaml"), "plain")).toEqual(plainData);
+    expect(findDiff(getPath("data1.yml"), getPath("data2.yaml"), "json")).toEqual(jsonData);
   });
 });
