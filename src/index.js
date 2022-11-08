855f47d18008be2c91db230ad7ccffaa74ffe128
Бабкина <babkinaolga2000@gmail.com>
2022-11-08 08:56:15 +0300

    исправление ошибок линтера

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
 import path from 'path';
 import fs from 'fs';
-import { fileURLToPath } from 'url';
 import parse from './parsers.js';
 import format from './formatters/index.js';
 import buildTree from './treeBuilder.js';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const extractFormat = (filePath) => path.parse(filePath).ext.slice(1);
 
 const buildFullPath = (filePath) => path.resolve(process.cwd(), filePath);
 
-const getData = (filePath) => parse(fs.readFileSync(filePath,'utf8'), extractFormat(filePath));
+const getData = (filePath) => parse(fs.readFileSync(filePath, 'utf8'), extractFormat(filePath));
 
 export default (filePath1, filePath2, outputFormat = 'stylish') => {
   const data1 = getData(buildFullPath(filePath1));
